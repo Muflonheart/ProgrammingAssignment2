caf8fa56b71dd6f2f18cb3977f69cc66d5c98756
Andrea Magni <amagni@gmail.com>
2020-10-18 08:49:07 +0300

    First attempt at solving the programming exercise on lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The structure is pretty similar to the MakeVector/Cachemean example
+## Instead of a mean it will cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function sets the values and has a nested function that 
+## calls to the parent environment to clear the chaced value 
 
 makeCacheMatrix <- function(x = matrix()) {
+  i=NULL
+  set <- function(y) {
+      x <<- y
+      i <<- NULL
+  }
+  get <- function(x)
+  setinv <- function(inverse) i <<-inverse
+  getinv <- function() i
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  }
 
-}
 
-
-## Write a short comment describing this function
+## This part is where the programs checks if there is cached value
+## and, if not, where the inverse matrix is calculate
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+          message("getting cached data")
+          return(j)
+        }
+        dat <- x$get()
+        i <- solve(dat, ...)
+        x$setinv(i)
+        i
 }
